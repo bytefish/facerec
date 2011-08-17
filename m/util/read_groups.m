3ba57f50e8a7b9e19a9e0119188d2ff18f2e16ac
Philipp Wagner <bytefish@gmx.de>
2011-08-17 20:40:32 +0200

    read_groups.m: read dataset with structure group/subject/image. LeaveOneClassOutCV.m: simple subject-independent cross validation for given groups and subjects.

---
+++ m/util/read_groups.m
@@ -0,0 +1,53 @@
+function [X y g group_names subject_names width height] = read_groups(path)
+	%% Read groups of images from a given path and return the Imagematrix X.
+	%%
+	%% Args:
+	%%   path: 
+	%%
+	%% Returns:
+	%%  X [dim x num_data] Array with images given in columns
+	%%  y [1 x num_data] Classes corresponding to images of X. 
+	%%  g [1 x num_data] Groups corresponding to classes of y.
+	%%  group_names {class_idx} names of a group
+	%%  subject_names {class_idx} names of the subject of a group
+	%%  width: width of the images
+	%%  height: height of the images
+	%%
+	%% Example:
+	%% 	[X y width height names] = read_images("./data/yalefaces")
+	%%
+	%% TODO add fixed image dimension, resizing images if necessary. 
+	%%
+	folder = list_files(path);
+	X = [];
+	y = [];
+	g = [];
+	group_names = {};
+	subject_names = {};
+	width = 0;
+	height = 0;
+	n = 0; % to remember class offset (because read_images returns c = {1,2,..,n})
+	gi = 1; % need to count if a folder is empty
+	for i=1:length(folder)
+		subject = folder{i};
+		group = [path, filesep, subject];
+		% no files to read? -- do nothing
+		if(length(list_files(group)) == 0)
+			continue;
+		endif
+		% else read group
+		[Xi yi width height names] = read_images(group);
+		% add class offset
+		yi = yi + n;
+		% add the data
+		X = [X, Xi];
+		y = [y, yi];
+		g = [g, repmat(gi, 1, size(yi,2))];
+		% names
+		group_names{gi} = subject;
+		subject_names{gi} = names;
+		% set new class offset and group
+		n = n + max(yi);
+		gi = gi+1;
+	endfor
+endfunction
---
+++ m/validation/LeaveOneClassOutCV.m
@@ -0,0 +1,46 @@
+function validation_result = LeaveOneClassOutCV(X, y, g, fun_train, fun_predict, print_debug)
+	%% Performs a Leave-One-Out-Cross validation.
+	%%
+	%% Args:
+	%%  g [1 x num_data] groups corresponding to classes of y
+	%%  see KFoldCV.m
+	
+	if(~exist("print_debug"))
+		print_debug = 0;
+	endif
+	
+	% shuffle idx
+	[d idx] = sort(rand(1, size(X,2)));
+	% shuffle X,y,g accordingly
+	X = X(:,idx);
+	y = y(idx);
+	g = g(idx);
+	
+	% init prediction results
+	tp = 0; fp = 0; tn = 0; fn = 0;
+	
+	% Perform Leave Class Out CV 
+	C = max(y); % y must be {1,2,3,...,C}
+	for i = 1:C
+		if(print_debug)
+			printf("Processing class %d/%d.\n",i,C);
+			fflush(stdout);
+		endif
+		% build indices
+		testIdx = find(y==i);
+		trainIdx = findclasses(y, [1:(i-1), (i+1):C]); 		% see findclasses.m (there's probably a better way around)
+		% learn the model (this time: by group!)
+		model = fun_train(X(:, trainIdx), g(trainIdx));
+		% test the model
+		for idx=testIdx
+			prediction = fun_predict(model, X(:,idx));
+			if(prediction == g(idx))
+				tp = tp + 1;
+			else
+				fp = fp + 1;
+			endif
+		endfor
+	endfor
+	% set result
+	validation_result = [tp fp tn fn];
+endfunction
