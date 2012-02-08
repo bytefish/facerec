ead4b89eca72db5f2a6541a4e376154acad511a4
Philipp Wagner <bytefish@gmx.de>
2012-02-08 21:09:48 +0100

    example updated.

--- py/apps/scripts/fisherfaces_example.py
+++ py/apps/scripts/fisherfaces_example.py
@@ -24,27 +24,18 @@ handler.setFormatter(formatter)
 logger = logging.getLogger("facerec")
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
-# load a dataset
-dataSet = DataSet("/home/philipp/facerec/data/c1")
+# load a dataset (e.g. AT&T Facedatabase)
+dataSet = DataSet("/home/philipp/facerec/data/at")
 # define Fisherfaces as feature extraction method
 feature = Fisherfaces()
-#from svmutil import *
-# define a SVM with RBF Kernel as classifier
-#param = svm_parameter("-q")
-#param.kernel_type = RBF
-#classifier = SVM(param=param)
 # define a 1-NN classifier with Euclidean Distance
 classifier = NearestNeighbor(dist_metric=EuclideanDistance(), k=1)
 # define the model as the combination
 model = PredictableModel(feature=feature, classifier=classifier)
-
-#best_parameter, results = grid_search(model, dataSet.data, dataSet.labels, k=5)
-#print results
 # show fisherfaces
-#model.compute(dataSet.data, dataSet.labels)
-#plot_eigenvectors(model.feature.eigenvectors, 1, sz=dataSet.data[0].shape, filename="/home/philipp/01.png")
-#plot_eigenvectors(model.feature.model2.eigenvectors, 1, sz=(dataSet.data[0].shape[0]-2,dataSet.data[0].shape[1]-2), filename="/home/philipp/01.png")
-# perform a 5-fold cross validation
+model.compute(dataSet.data, dataSet.labels)
+plot_eigenvectors(model.feature.eigenvectors, 10, sz=dataSet.data[0].shape, filename="fisherfaces.pdf")
+# perform a 10-fold cross validation
 cv = KFoldCrossValidation(model, k=10)
 cv.validate(dataSet.data, dataSet.labels)
 print cv
