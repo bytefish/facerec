3eb856681d21e0fc6985259cfe03b631e5b3d394
Philipp Wagner <bytefish@gmx.de>
2012-05-25 17:36:07 +0200

    Fixed SVM import.

--- py/facerec/classifier.py
+++ py/facerec/classifier.py
@@ -44,7 +44,56 @@ class NearestNeighbor(AbstractClassifier):
 # libsvm
 try:
 	from svmutil import *
-	# for suppressing output
+	import sys
+	from StringIO import StringIO
+	bkp_stdout=sys.stdout
+
+	class SVM(AbstractClassifier):
+		"""
+		This class is just a simple wrapper to use libsvm in the 
+		CrossValidation module. If you don't use this framework
+		use the validation methods coming with LibSVM, they are
+		much easier to access (simply pass the correct class 
+		labels in svm_predict and you are done...).
+	
+		The grid search method in this class is somewhat similar
+		to libsvm grid.py, as it performs a parameter search over
+		a logarithmic scale.	Again if you don't use this framework, 
+		use the libsvm tools as they are much easier to access.
+	
+		Please keep in mind to normalize your input data, as expected
+		for the model. There's no way to assume a generic normalization
+		step.
+		"""
+	
+		def __init__(self, param=None):
+			AbstractClassifier.__init__(self)
+			self.logger = logging.getLogger("facerec.classifier.SVM")
+			self.param = param
+			self.svm = svm_model()
+			self.param = param
+			if self.param is None:
+				self.param = svm_parameter("-q")
+		
+		def compute(self, X, y):
+			self.logger.debug("SVM TRAINING (C=%.2f,gamma=%.2f,p=%.2f,nu=%.2f,coef=%.2f,degree=%.2f)" % (self.param.C, self.param.gamma, self.param.p, self.param.nu, self.param.coef0, self.param.degree))
+			# turn data into a row vector (needed for libsvm)
+			X = asRowMatrix(X)
+			y = np.asarray(y)
+			problem = svm_problem(y, X.tolist())		
+			self.svm = svm_train(problem, self.param)
+			self.y = y
+		
+		def predict(self, X):
+			X = np.asarray(X).reshape(1,-1)
+			sys.stdout=StringIO() 
+			p_lbl, p_acc, p_val = svm_predict([0], X.tolist(), self.svm)
+			sys.stdout=bkp_stdout
+			return int(p_lbl[0])
+		
+		def __repr__(self):		
+			return "Support Vector Machine (kernel_type=%s, C=%.2f,gamma=%.2f,p=%.2f,nu=%.2f,coef=%.2f,degree=%.2f)" % (KERNEL_TYPE[self.param.kernel_type], self.param.C, self.param.gamma, self.param.p, self.param.nu, self.param.coef0, self.param.degree)
+
 except ImportError:
 	logger = logging.getLogger("facerec.classifier.SVM")
 	logger.debug("Import Error: libsvm bindings not available.")
@@ -52,51 +101,4 @@ except:
 	logger = logging.getLogger("facerec.classifier.SVM")
 	logger.debug("Import Error: libsvm bindings not available.")
 
-import sys
-from StringIO import StringIO
-# function handle to stdout
-bkp_stdout=sys.stdout
 
-class SVM(AbstractClassifier):
-	"""
-	This class is just a simple wrapper to use libsvm in the 
-	CrossValidation module. If you don't use this framework
-	use the validation methods coming with LibSVM, they are
-	much easier to access (simply pass the correct class 
-	labels in svm_predict and you are done...).
-	
-	The grid search method in this class is somewhat similar
-	to libsvm grid.py, as it performs a parameter search over
-	a logarithmic scale.	Again if you don't use this framework, 
-	use the libsvm tools as they are much easier to access.
-	
-	Please keep in mind to normalize your input data, as expected
-	for the model. There's no way to assume a generic normalization
-	step.
-	"""
-	
-	def __init__(self, param=svm_parameter("-q")):
-		AbstractClassifier.__init__(self)
-		self.logger = logging.getLogger("facerec.classifier.SVM")
-		self.param = param
-		self.svm = svm_model()
-		self.param = param
-		
-	def compute(self, X, y):
-		self.logger.debug("SVM TRAINING (C=%.2f,gamma=%.2f,p=%.2f,nu=%.2f,coef=%.2f,degree=%.2f)" % (self.param.C, self.param.gamma, self.param.p, self.param.nu, self.param.coef0, self.param.degree))
-		# turn data into a row vector (needed for libsvm)
-		X = asRowMatrix(X)
-		y = np.asarray(y)
-		problem = svm_problem(y, X.tolist())		
-		self.svm = svm_train(problem, self.param)
-		self.y = y
-		
-	def predict(self, X):
-		X = np.asarray(X).reshape(1,-1)
-		sys.stdout=StringIO() 
-		p_lbl, p_acc, p_val = svm_predict([0], X.tolist(), self.svm)
-		sys.stdout=bkp_stdout
-		return int(p_lbl[0])
-		
-	def __repr__(self):		
-		return "Support Vector Machine (kernel_type=%s, C=%.2f,gamma=%.2f,p=%.2f,nu=%.2f,coef=%.2f,degree=%.2f)" % (KERNEL_TYPE[self.param.kernel_type], self.param.C, self.param.gamma, self.param.p, self.param.nu, self.param.coef0, self.param.degree)
