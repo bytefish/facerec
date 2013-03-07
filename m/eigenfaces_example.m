b634d4d74a49f4918664bf636a0e983f514ad022
Philipp Wagner <bytefish@gmx.de>
2013-03-07 07:39:32 +0100

    Added check to prevent plotting 3D if not possible.

--- m/eigenfaces_example.m
+++ m/eigenfaces_example.m
@@ -5,32 +5,34 @@ addpath (genpath ('.'));
 [X y width height names] = read_images('/home/philipp/facerec/data/yalefaces_recognition');
 
 % compute a model
-eigenface = eigenfaces(X,y,100);
+eigenface = eigenfaces(X,y,30);
 
 % plot the first (atmost) 16 eigenfaces
 figure; 
 title('Eigenfaces (AT&T Facedatabase)');
 hold on;
 for i=1:min(16, size(eigenface.W,2))
-    subplot(4,4,i);
-    comp = cvtGray(eigenface.W(:,i), width, height);
-    imshow(comp);
-    colormap(jet(256));
-    title(sprintf('Eigenface #%i', i));
+  subplot(4,4,i);
+  comp = cvtGray(eigenface.W(:,i), width, height);
+  imshow(comp);
+  colormap(jet(256));
+  title(sprintf('Eigenface #%i', i));
 end
 
 %% 2D plot of projection (add the classes you want)
 figure; hold on;
 for i = findclasses(eigenface.y, [1,2,3])
-	text(eigenface.P(1,i), eigenface.P(2,i), num2str(eigenface.y(i)));
+  text(eigenface.P(1,i), eigenface.P(2,i), num2str(eigenface.y(i)));
 end
 
 %% 3D plot of projection (first three classes, add those you want)
-figure; hold on;
-for i = findclasses(eigenface.y, [1,2,3])
-	% LineSpec: red dots 'r.'
-	plot3(eigenface.P(1,i), eigenface.P(2,i), eigenface.P(3,i), 'r.'), view(45,-45);
-	text(eigenface.P(1,i), eigenface.P(2,i), eigenface.P(3,i), num2str(eigenface.y(i)));
+if(rows(eigenface.P) >= 3)
+  figure; hold on;
+  for i = findclasses(eigenface.y, [1,2,3])
+    % LineSpec: red dots 'r.'
+    plot3(eigenface.P(1,i), eigenface.P(2,i), eigenface.P(3,i), 'r.'), view(45,-45);
+    text(eigenface.P(1,i), eigenface.P(2,i), eigenface.P(3,i), num2str(eigenface.y(i)));
+  end
 end
 
 pause;
--- m/fisherfaces_example.m
+++ m/fisherfaces_example.m
@@ -10,25 +10,27 @@ fisherface = fisherfaces(X,y);
 % plot fisherfaces
 figure; hold on;
 for i=1:min(16, size(fisherface.W,2))
-    subplot(4,4,i);
-    comp = cvtGray(fisherface.W(:,i), width, height);
-    imshow(comp);
-    colormap(jet(256));
-    title(sprintf('Fisherface #%i', i));
+  subplot(4,4,i);
+  comp = cvtGray(fisherface.W(:,i), width, height);
+  imshow(comp);
+  colormap(jet(256));
+  title(sprintf('Fisherface #%i', i));
 end
 
 %% 2D plot of projection (first three classes)
 figure; hold on;
 for i = findclasses(fisherface.y, [1,2,3])
-	text(fisherface.P(1,i), fisherface.P(2,i), num2str(fisherface.y(i)));
+  text(fisherface.P(1,i), fisherface.P(2,i), num2str(fisherface.y(i)));
 end
 
 %% 3D plot of projection (first three classes)
-figure; hold on;
-for i = findclasses(fisherface.y, [1,2,3])
-	% LineSpec: red dots 'r.'
-	plot3(fisherface.P(1,i), fisherface.P(2,i), fisherface.P(3,i), 'r.'), view(45,-45);
-	text(fisherface.P(1,i), fisherface.P(2,i), fisherface.P(3,i), num2str(fisherface.y(i)));
+if(rows(fisherface.P) >= 3)
+  figure; hold on;
+  for i = findclasses(fisherface.y, [1,2,3])
+    % LineSpec: red dots 'r.'
+    plot3(fisherface.P(1,i), fisherface.P(2,i), fisherface.P(3,i), 'r.'), view(45,-45);
+    text(fisherface.P(1,i), fisherface.P(2,i), fisherface.P(3,i), num2str(fisherface.y(i)));
+  end
 end
 
 pause;
