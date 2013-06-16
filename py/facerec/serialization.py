1f9f06896723e700840862785530bc80cbd2dd9f
Philipp Wagner <bytefish@gmx.de>
2013-06-16 22:37:41 +0200

    Using cPickle instead of Pickle. Thanks to @softforge for the hint.

--- py/facerec/serialization.py
+++ py/facerec/serialization.py
@@ -1,12 +1,12 @@
-import pickle
+import cPickle
 
 def save_model(filename, model):
     output = open(filename, 'wb')
-    pickle.dump(model, output)
+    cPickle.dump(model, output)
     output.close()
     
 def load_model(filename):
     pkl_file = open(filename, 'rb')
-    res = pickle.load(pkl_file)
+    res = cPickle.load(pkl_file)
     pkl_file.close()
     return res
