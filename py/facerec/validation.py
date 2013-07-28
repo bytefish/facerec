d284556a4a88985a8ccbdb1809dcdd6c88c36014
Philipp Wagner <bytefish@gmx.de>
2013-07-28 21:25:49 +0200

    Starting refactoring of the Validation module.

--- py/facerec/validation.py
+++ py/facerec/validation.py
@@ -6,6 +6,18 @@ import logging
 from facerec.model import PredictableModel
 from facerec.classifier import AbstractClassifier
 
+# TODO The evaluation of a model should be completely moved to the generic ValidationStrategy. The specific Validation 
+#       implementations should only care about partition the data, which would make a lot sense. Currently it is not 
+#       possible to calculate the true_negatives and false_negatives with the way the predicitions are generated and 
+#       data is prepared.
+#       
+#     The mentioned problem makes a change in the PredictionResult necessary, which basically means refactoring the 
+#       entire framework. The refactoring is planned, but I can't give any details as time of writing.
+#
+#     Please be careful, when referring to the Performance Metrics at the moment, only the Precision is implemented,
+#       and the rest has no use at the moment. Due to the missing refactoring discussed above.
+#
+
 def shuffle(X, y):
     """ Shuffles two arrays by column (len(X) == len(y))
         
@@ -23,7 +35,7 @@ def shuffle(X, y):
     X = [X[i] for i in idx]
     y = y[idx]
     return (X, y)
-
+    
 def slice_2d(X,rows,cols):
     """
     
@@ -44,7 +56,42 @@ def slice_2d(X,rows,cols):
     """
     return [X[i][j] for j in cols for i in rows]
 
-class Validation(object):
+def precision(true_positives, false_positives):
+    """Returns the precision, calculated as:
+        
+        true_positives/(true_positives+false_positives)
+        
+    """
+    return accuracy(true_positives, 0, false_positives, 0)
+    
+def accuracy(true_positives, true_negatives, false_positives, false_negatives):
+    """Returns the accuracy, calculated as:
+    
+        (true_positives+true_negatives)/(true_positives+false_positives+true_negatives+false_negatives)
+        
+    """
+    true_positives = float(true_positives)
+    true_negatives = float(true_negatives)
+    false_positives = float(false_positives)
+    false_negatives = float(false_negatives)
+    if (true_positives + true_negatives + false_positives + false_negatives) < 1e-15:
+       return 0.0
+    return (true_positives+true_negatives)/(true_positives+false_positives+true_negatives+false_negatives)
+
+class ValidationResult(object):
+    """Holds a validation result.
+    """
+    def __init__(self, true_positives, true_negatives, false_positives, false_negatives)
+        self.true_positives = true_positives
+        self.true_negatives = true_negatives
+        self.false_positives = false_positives
+        self.false_negatives = false_negatives
+        
+    def __repr__(self):
+        print "Precision = ", precision(self.true_positives, self.false_positives)
+        print "Accuracy = ", accuracy(self.true_positives, self.true_negatives, self.false_positives, self.false_negatives)
+    
+class ValidationStrategy(object):
     """ Represents a generic Validation kernel for all Validation strategies.
     
     Attributes:
@@ -58,109 +105,30 @@ class Validation(object):
     def __init__(self, model):
         """    
         Initialize validation with empty results.
+        
+        Args:
+        
+            model [PredictableModel] The model, which is going to be validated.
         """
-        # make sure we can predict on this one
         if not isinstance(model,PredictableModel):
             raise TypeError("Validation can only validate the type PredictableModel.")
         self.model = model
-        self._results = np.empty((0,4), np.int)
+        self.validation_results = []
     
-    def add(self, result):
-        """ Adds a validation result [tp,fp,tn,fn] to this Validation object.
-        
-        Args:
-        
-            result [list] [tp, fp, tn, fn]
-        """
-        if len(result) != 4:
-            return
-        self._results = np.vstack((self._results, result))
-        
-    def at(self, idx, row=None):
-        """ 
-        
-        Returns idx columns of the results list (all if None given).
-        
-        Args:
-        
-            idx [int] Column to return (0 == tp, 1 == fp, 2 == tn, 3 == fn)
-            row [int] Rows to include
-            
-        Returns: 
-            List with sliced results.
-        """
-        if row is None:
-            row = range(0,self._results.shape[0])
-        if idx is None:
-            idx = range(0, self._results.shape[1])
-        ret = self._results[row, idx]
-        if np.size(ret) == 1:
-            return np.int(ret)
-        return ret
+    def add(self, validation_result):
+        self.validation_results.append(validation_result)
         
     def validate(self, X, y):
         raise NotImplementedError("Every Validation module must implement the validate method!")
-
-    @property
-    def tp(self):
-        return self.at(0)
-
-    @property
-    def fp(self):
-        return self.at(1)
-    
-    @property
-    def tn(self):
-        return self.at(2)
-    
-    @property
-    def fn(self):
-        return self.at(3)
         
-    @property
-    def tps(self):
-        return np.sum(self.at(0))
-    
-    @property
-    def fps(self):
-        return np.sum(self.at(1))
-    
-    @property
-    def tns(self):
-        return np.sum(self.at(2))
+    def print_results(self):
+        for validation_result in self.validation_results:
+            print validation_result
 
-    @property
-    def fns(self):
-        return np.sum(self.at(3))
-
-    @property
-    def runs(self):
-        return self._results.shape[0]
-    
-    @property
-    def results(self):
-        return self._results
-
-    @property
-    def accuracy(self):
-        tps = float(self.tps)
-        fps = float(self.fps)
-        if (tps+fps) < 1e-15:
-            return 0.0
-        return (tps/(tps+fps))
-        
-    @property
-    def std_accuracy(self):
-        try:
-            accs = (np.array(self.tp, dtype=np.float32)/(np.array(self.tp+self.fp, dtype=np.float32)))
-        except:
-            return 0.0
-        return np.std(accs)
-        
     def __repr__(self):
-        return "Validation Kernel (model=%s, runs=%s, accuracy=%.2f%%, std(accuracy)=%.2f%%, tp=%s, fp=%s, tn=%s, fn=%s)" % (self.model, self.runs, (self.accuracy*100), (self.std_accuracy*100), self.tps, self.fps, self.tns, self.fns)
+        return "Validation Kernel (model=%s)" % (self.model)
         
-class KFoldCrossValidation(Validation):
+class KFoldCrossValidation(ValidationStrategy):
     """ 
     
     Divides the Data into 10 equally spaced and non-overlapping folds for training and testing.
@@ -173,22 +141,14 @@ class KFoldCrossValidation(Validation):
     c2 | A  B  B |  c2 | B  A  B |  c2 | B  B  A |
     
     Please note: If there are less than k observations in a class, k is set to the minimum of observations available through all classes.
-    
-    Arguments:
-    
-        model [Model] model for this validation
-        ... see [Validation]
     """
-    def __init__(self, model, k=10, results_per_fold=False):
+    def __init__(self, model, k=10):
         """
         Args:
-            model [Model] model to perform the validation on
             k [int] number of folds in this k-fold cross-validation (default 10)
-            results_per_fold [bool] store results per fold (default False)
         """
         super(KFoldCrossValidation, self).__init__(model=model)
         self.k = k
-        self.results_per_fold = results_per_fold
         self.logger = logging.getLogger("facerec.validation.KFoldCrossValidation")
 
     def validate(self, X, y):
@@ -217,7 +177,7 @@ class KFoldCrossValidation(Validation):
 
         foldSize = int(math.floor(n/self.k))
         
-        tp, fp, tn, fn = (0,0,0,0)
+        true_positives, false_positives, true_negatives, false_negatives = (0,0,0,0)
         for i in range(0,self.k):
         
             self.logger.info("Processing fold %d/%d." % (i+1, self.k))
@@ -234,27 +194,25 @@ class KFoldCrossValidation(Validation):
             ytrain = y[trainIdx]
                         
             self.model.compute(Xtrain, ytrain)
-
+            
+            # TODO I have to add the true_negatives and false_negatives. Models also need to support it,
+            # so we should use a PredictionResult, instead of trying to do this by simply comparing
+            # the predicted and actual class.
+            #
+            # This is inteneded of the next version! Feel free to contribute.
             for j in testIdx:
                 prediction = self.model.predict(X[j])[0]
                 if prediction == y[j]:
-                    tp = tp + 1
+                    true_positives = true_positives + 1
                 else:
-                    fp = fp + 1
-
-            # add result for this foldIndex
-            if self.results_per_fold:
-                self.add([tp, fp, tn, fn])
-                tp, fp, tn, fn = (0,0,0,0)
+                    false_positives = false_positives + 1
                     
-        # add k-fold cv results
-        if not self.results_per_fold:
-            self.add([tp, fp, tn, fn])
+        self.add(ValidationResult(true_positives, true_negatives, false_positives, false_negatives))
     
     def __repr__(self):
-        return "k-Fold Cross Validation (model=%s, k=%s, runs=%s, accuracy=%.2f%%, std(accuracy)=%.2f%%, tp=%s, fp=%s, tn=%s, fn=%s)" % (self.model, self.k, self.runs, (self.accuracy*100.0), (self.std_accuracy*100.0), self.tps, self.fps, self.tns, self.fns)
+        return "k-Fold Cross Validation (model=%s, k=%s)" % (self.model, self.k)
 
-class LeaveOneOutCrossValidation(Validation):
+class LeaveOneOutCrossValidation(ValidationStrategy):
     """ Leave-One-Cross Validation (LOOCV) uses one observation for testing and the rest for training a classifier:
 
         o0 o1 o2        o0 o1 o2        o0 o1 o2           o0 o1 o2
@@ -284,7 +242,7 @@ class LeaveOneOutCrossValidation(Validation):
             y [1 x num_data] classes
         """
         #(X,y) = shuffle(X,y)
-        tp, fp, tn, fn = (0,0,0,0)
+        true_positives, false_positives, true_negatives, false_negatives = (0,0,0,0)
         n = y.shape[0]
         for i in range(0,n):
             
@@ -305,15 +263,15 @@ class LeaveOneOutCrossValidation(Validation):
             # get prediction
             prediction = self.model.predict(X[i])[0]
             if prediction == y[i]:
-                tp = tp + 1
+                true_positives = true_positives + 1
             else:
-                fp = fp + 1
+                false_positives = false_positives + 1
         self.add([tp, fp, tn, fn])
     
     def __repr__(self):
-        return "Leave-One-Out Cross Validation (model=%s, runs=%d, accuracy=%.2f%%, tp=%s, fp=%s, tn=%s, fn=%s)" % (self.model, self.runs, (self.accuracy * 100.0), self.tps, self.fps, self.tns, self.fns)
+        return "Leave-One-Out Cross Validation (model=%s)" % (self.model)
 
-class LeaveOneClassOutCrossValidation(Validation):
+class LeaveOneClassOutCrossValidation(ValidationStrategy):
     """ Leave-One-Cross Validation (LOOCV) uses one observation for testing and the rest for training a classifier:
 
         o0 o1 o2        o0 o1 o2        o0 o1 o2           o0 o1 o2
@@ -337,7 +295,7 @@ class LeaveOneClassOutCrossValidation(Validation):
         
     def validate(self, X, y, g):
         #(X,y) = shuffle(X,y)
-        tp, fp, tn, fn = (0,0,0,0)
+        true_positives, false_positives, true_negatives, false_negatives = (0,0,0,0)
         
         for i in range(0,len(np.unique(y))):
             self.logger.info("Validating Class %s." % i)
@@ -348,33 +306,33 @@ class LeaveOneClassOutCrossValidation(Validation):
             Xtrain = [X[t] for t in trainIdx]
             gtrain = g[trainIdx]
             
-            # compute the model (this time on the group!)
+            # Compute the model, this time on the group:
             self.model.compute(Xtrain, gtrain)
             
             for j in testIdx:
                 # get prediction
                 prediction = self.model.predict(X[j])[0]
                 if prediction == g[j]:
-                    tp = tp + 1
+                    true_positives = true_positives + 1
                 else:
-                    fp = fp + 1
-        self.add([tp, fp, tn, fn])
+                    false_positives = false_positives + 1
+        self.add(ValidationResult(true_positives, true_negatives, false_positives, false_negatives))
     
     def __repr__(self):
-        return "Leave-One-Class-Out Cross Validation (model=%s, runs=%d, accuracy=%.2f%%, tp=%s, fp=%s, tn=%s, fn=%s)" % (self.model, self.runs, (self.accuracy * 100.0), self.tps, self.fps, self.tns, self.fns)
+        return "Leave-One-Class-Out Cross Validation (model=%s)" % (self.model)
 
-class SimpleValidation(Validation):
-    """
+class SimpleValidation(ValidationStrategy):
+    """Implements a simple Validation, which allows you to partition the data yourself.
     """
     def __init__(self, model):
         """
         Args:
-            model [Model] model to perform the validation on
+            model [PredictableModel] model to perform the validation on
         """
         super(SimpleValidation, self).__init__(model=model)
         self.logger = logging.getLogger("facerec.validation.SimpleValidation")
             
-    def validate(self, X, y, trainIdx, testIdx):
+    def validate(self, X, y, trainIndices, testIndices):
         """
         Performs a validation given training data and test data. User is responsible for non-overlapping assignment of indices.
 
@@ -383,26 +341,25 @@ class SimpleValidation(Validation):
             y [1 x num_data] classes
         """
         self.logger.info("Simple Validation.")
-        # build training data/test from given indices
-        Xtrain = [X[t] for t in trainIdx]
-        ytrain = y[trainIdx]
         
-        # now compute the model
+        Xtrain = [X[t] for t in trainIndices]
+        ytrain = y[trainIndices]
+        
         self.model.compute(Xtrain, ytrain)
 
         self.logger.debug("Model computed.")
 
-        tp, fp, tn, fn = (0,0,0,0)
+        true_positives, false_positives, true_negatives, false_negatives = (0,0,0,0)
         count = 0
-        for i in testIdx:
-            self.logger.debug("Predicting %s/%s." % (count, len(testIdx)))
+        for i in testIndices:
+            self.logger.debug("Predicting %s/%s." % (count, len(testIndices)))
             prediction = self.model.predict(X[i])[0]
             if prediction == y[i]:
-                tp = tp + 1
+                true_positives = true_positives + 1
             else:
-                fp = fp + 1
+                false_positives = false_positives + 1
             count = count + 1
-        self.add([tp, fp, tn, fn])
+        self.add(ValidationResult(true_positives, true_negatives, false_positives, false_negatives))
         
     def __repr__(self):
-        return "Simple Validation (model=%s, runs=%s, accuracy=%.2f%%, std(accuracy)=%.2f%%, tp=%s, fp=%s, tn=%s, fn=%s)" % (self.model, self.runs, (self.accuracy*100.0), (self.std_accuracy*100.0), self.tps, self.fps, self.tns, self.fns)
+        return "Simple Validation (model=%s)" % (self.model)
