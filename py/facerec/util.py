cc1a3ef63ca63378314734187414211e7894421f
Philipp Wagner <bytefish@gmx.de>
2013-08-14 13:38:09 +0200

    read_images used in almost every script has been moved to util.

--- py/facerec/util.py
+++ py/facerec/util.py
@@ -14,6 +14,41 @@ def read_image(filename):
         print "Cannot open image."
     return imarr
 
+def read_images(path, sz=None):
+    """Reads the images in a given folder, resizes images on the fly if size is given.
+
+    Args:
+        path: Path to a folder with subfolders representing the subjects (persons).
+        sz: A tuple with the size Resizes 
+
+    Returns:
+        A list [X,y]
+
+            X: The images, which is a Python list of numpy arrays.
+            y: The corresponding labels (the unique number of the subject, person) in a Python list.
+    """
+    c = 0
+    X,y = [], []
+    for dirname, dirnames, filenames in os.walk(path):
+        for subdirname in dirnames:
+            subject_path = os.path.join(dirname, subdirname)
+            for filename in os.listdir(subject_path):
+                try:
+                    im = Image.open(os.path.join(subject_path, filename))
+                    im = im.convert("L")
+                    # resize to given size (if given)
+                    if (sz is not None):
+                        im = im.resize(self.sz, Image.ANTIALIAS)
+                    X.append(np.asarray(im, dtype=np.uint8))
+                    y.append(c)
+                except IOError, (errno, strerror):
+                    print "I/O error({0}): {1}".format(errno, strerror)
+                except:
+                    print "Unexpected error:", sys.exc_info()[0]
+                    raise
+            c = c+1
+    return [X,y]
+
 def asRowMatrix(X):
     """
     Creates a row-matrix from multi-dimensional data items in list l.
