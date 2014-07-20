c52e788e6dba6aada959a70ec06d003e588723fb
Philipp Wagner <bytefish@gmx.de>
2014-07-20 20:03:09 +0200

    Adding a new project for VideoFaceRecognition
    I want to keep the old projects clean, so I could reuse them for a blog
    post.

---
+++ android/VideoFaceRecognition/app/src/main/java/org/bytefish/videofacerecognition/app/CameraActivity.java
@@ -0,0 +1,163 @@
+/**
+ * Copyright (c) 2014, Philipp Wagner <bytefish(at)gmx(dot)de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.bytefish.videofacerecognition.app;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.Face;
+import android.hardware.Camera.FaceDetectionListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.OrientationEventListener;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.ViewGroup.LayoutParams;
+
+
+public class CameraActivity extends Activity
+        implements SurfaceHolder.Callback {
+
+    public static final String TAG = CameraActivity.class.getSimpleName();
+
+    private Camera mCamera;
+    private int mOrientation;
+    private OrientationEventListener mOrientationEventListener;
+    private int mDisplayRotation;
+    private int mDisplayOrientation;
+    private SurfaceView mView;
+    private FaceOverlayView mFaceView;
+
+    /**
+     * Notifies the Surfaces on OrientationChanges.
+     */
+    private class SimpleOrientationEventListener extends OrientationEventListener {
+
+        public SimpleOrientationEventListener(Context context) {
+            super(context, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        @Override
+        public void onOrientationChanged(int orientation) {
+            mOrientation = orientation;
+        }
+    }
+
+    /**
+     * The FaceDetectionListener simply passes the faces to the OverlayView for now.
+     */
+    private FaceDetectionListener faceDetectionListener = new FaceDetectionListener() {
+        @Override
+        public void onFaceDetection(Face[] faces, Camera camera) {
+            Log.d("onFaceDetection", "Number of Faces:" + faces.length);
+            mFaceView.setFaces(faces, mOrientation);
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mView = new SurfaceView(this);
+        setContentView(mView);
+        // Now create the OverlayView:
+        mFaceView = new FaceOverlayView(this);
+        addContentView(mFaceView, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        // Create and Start the OrientationListener:
+        mOrientationEventListener = new SimpleOrientationEventListener(this);
+        mOrientationEventListener.enable();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        SurfaceHolder holder = mView.getHolder();
+        holder.addCallback(this);
+    }
+
+    @Override
+    protected void onPause() {
+        mOrientationEventListener.disable();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        mOrientationEventListener.enable();
+        super.onResume();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        mCamera = Camera.open();
+        mCamera.setFaceDetectionListener(faceDetectionListener);
+        mCamera.startFaceDetection();
+        try {
+            mCamera.setPreviewDisplay(surfaceHolder);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not preview the image.", e);
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+        // We have no surface, return immediately:
+        if (surfaceHolder.getSurface() == null) {
+            return;
+        }
+        // Try to stop the current preview:
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e) {
+            // Ignore...
+        }
+        // Get the supported preview sizes:
+        Camera.Parameters parameters = mCamera.getParameters();
+        List<Camera.Size> previewSizes = parameters.getSupportedPreviewSizes();
+        Camera.Size previewSize = previewSizes.get(0);
+        // And set them:
+        parameters.setPreviewSize(previewSize.width, previewSize.height);
+        mCamera.setParameters(parameters);
+        // Now set the display orientation for the camera. Can we do this differently?
+        mDisplayRotation = Util.getDisplayRotation(CameraActivity.this);
+        mDisplayOrientation = Util.getDisplayOrientation(mDisplayRotation, 0);
+        mCamera.setDisplayOrientation(mDisplayOrientation);
+        if (mFaceView != null) {
+            mFaceView.setDisplayOrientation(mDisplayOrientation);
+        }
+        // Finally start the camera preview again:
+        mCamera.startPreview();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        mCamera.release();
+        mCamera.setFaceDetectionListener(null);
+        mCamera.setErrorCallback(null);
+        mCamera = null;
+    }
+}
---
+++ android/VideoFaceRecognition/app/src/main/java/org/bytefish/videofacerecognition/app/FaceOverlayView.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2014, Philipp Wagner <bytefish(at)gmx(dot)de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted 
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of 
+ * conditions and the following disclaimer in the documentation and/or other materials provided 
+ * with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.bytefish.videofacerecognition.app;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.hardware.Camera.Face;
+import android.view.MotionEvent;
+import android.view.OrientationEventListener;
+import android.view.Surface;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * This class is a simple View to display the faces.
+ */
+public class FaceOverlayView extends View {
+
+    private Paint mPaint;
+    private int mDisplayOrientation;
+    private int mOrientation;
+    private Face[] mFaces;
+
+    public FaceOverlayView(Context context) {
+        super(context);
+        initialize();
+    }
+
+    private void initialize() {
+        // We want a green box around the face:
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(Color.GREEN);
+        mPaint.setAlpha(128);
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+    }
+
+    public void setFaces(Face[] faces, int orientation) {
+        mFaces = faces;
+        mOrientation = orientation;
+        invalidate();
+    }
+
+    public void setDisplayOrientation(int displayOrientation) {
+        mDisplayOrientation = displayOrientation;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mFaces != null && mFaces.length > 0) {
+            Matrix matrix = new Matrix();
+            Util.prepareMatrix(matrix, false, mDisplayOrientation, getWidth(), getHeight());
+            canvas.save();
+            matrix.postRotate(mOrientation);
+            canvas.rotate(-mOrientation);
+            RectF rectF = new RectF();
+            for (Face face : mFaces) {
+                rectF.set(face.rect);
+                matrix.mapRect(rectF);
+                canvas.drawRect(rectF, mPaint);
+            }
+            canvas.restore();
+        }
+    }
+}
