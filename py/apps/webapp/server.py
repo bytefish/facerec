8e7d12e5b340e71c812347e659133422a2c79e26
Philipp Wagner <bytefish@gmx.de>
2014-08-10 19:09:34 +0200

    updating initial webapp project
    The old server was a test. It is now replaced by the new server, which
    makes extending the Web service a lot easier.

--- py/apps/webapp/server.py
+++ py/apps/webapp/server.py
@@ -30,6 +30,7 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+
 import cStringIO
 import base64
 # try to import the PIL Image
@@ -37,132 +38,225 @@ try:
     from PIL import Image
 except ImportError:
     import Image
+
 # Flask imports:
-from flask import Flask, request, json, abort, make_response, Response, jsonify
+from flask import Flask, request, request_finished, json, abort, make_response, Response, jsonify
+# facerec imports
 # facerec imports:
 import sys
-sys.path.append("../..")
-# facerec imports
+sys.path.append("../../..")
 from facerec.model import PredictableModel
 from facerec.lbp import ExtendedLBP
 from facerec.feature import SpatialHistogram
 from facerec.distance import ChiSquareDistance
 from facerec.classifier import NearestNeighbor
+
 # logging
 import logging
+from logging.handlers import RotatingFileHandler
 
-# Test run:
-# Encode image as Base64:
-#   openssl enc -base64 -in D:\facerec\data\c1\crop_arnold_schwarzenegger\crop_01.jpg
-# Send request (Linux/Cygwin):
-#   curl -i -H "Content-Type: application/json" -X POST -d '{"name":"Arnie", "image":""}' http://localhost:5000/add
-# Send request (Windows):
-# curl -i -H "Content-Type: application/json" -X POST -d "{"""name""":"""Arnie""", """image""":"""base64image"""}" http://localhost:5000/add
+# the webserver recognition module
+import recognition
 
 # The main application: 
 app = Flask(__name__)
 
-# Limit the maximum image size:
-IMG_MAX_SIZE = (128,128)
+# This is a list of errors the Webservice returns. You can come up
+# with new error codes and plug them into the API.
+#
+# An example JSON response for an error looks like this:
+#
+#   { "status" : failed, "message" : "IMAGE_DECODE_ERROR", "code" : 10 }
+#
+# If there are multiple errors, only the first error is considered.
 
+IMAGE_DECODE_ERROR = 10
+IMAGE_RESIZE_ERROR = 11
+PREDICTION_ERROR = 12
+SERVICE_TEMPORARY_UNAVAILABLE = 20
+UNKNOWN_ERROR = 21
+INVALID_FORMAT = 30
+INVALID_API_KEY = 31
+INVALID_API_TOKEN = 32
+MISSING_ARGUMENTS = 40
 
-""" Probably useful for Server exceptions, probably not
-"""
-class WebAppException(Exception):
-    pass
+errors = {
+    IMAGE_DECODE_ERROR : "IMAGE_DECODE_ERROR",
+    IMAGE_RESIZE_ERROR  : "IMAGE_RESIZE_ERROR",
+    SERVICE_TEMPORARY_UNAVAILABLE	: "SERVICE_TEMPORARILY_UNAVAILABLE",
+    PREDICTION_ERROR : "PREDICTION_ERROR",
+    UNKNOWN_ERROR : "UNKNOWN_ERROR",
+    INVALID_FORMAT : "INVALID_FORMAT",
+    INVALID_API_KEY : "INVALID_API_KEY",
+    INVALID_API_TOKEN : "INVALID_API_TOKEN",
+    MISSING_ARGUMENTS : "MISSING_ARGUMENTS"
+}
+
+# Setup the logging for the server, so we can log all exceptions
+# away. We also want to acquire a logger for the facerec framework,
+# so we can be sure, that all logging goes into one place.
+LOG_FILENAME = 'serverlog.log'
+LOG_BACKUP_COUNT = 5
+LOG_FILE_SIZE_BYTES = 50 * 1024 * 1024
+
+def init_logger(app):
+    handler = RotatingFileHandler(LOG_FILENAME, maxBytes=LOG_FILE_SIZE_BYTES, backupCount=LOG_BACKUP_COUNT)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    loggers = [app.logger, logging.getLogger('facerec')]
+    for logger in loggers:
+        logger.addHandler(handler)
+
+# Bring the model variable into global scope. This might be
+# dangerous in Flask, I am trying to figure out, which is the
+# best practice here. 
+
+# Initializes the Flask application, which is going to 
+# add the loggers, load the initial facerec model and 
+# all of this.
+def init_app(app):
+    init_logger(app)
+
+init_app(app)
+
+@app.before_request
+def log_request():
+    app.logger.debug("Request: %s %s", request.method, request.url)
     
-# Define a model, that supports updating itself. This is necessary,
-# so we don't need to retrain the entire model for each input image.
-# This is not suitable for all models, it may be limited to Local 
-# Binary Patterns for the current framework:    
-class UpdatableModel(PredictableModel):
-    """ Subclasses the PredictableModel to store some more
-        information, so we don't need to pass the dataset
-        on each program call...
-    """
+# The WebAppException might be useful. It enables us to 
+# throw exceptions at any place in the application and give the user
+# a 400 error code.
+class WebAppException(Exception):
 
-    def __init__(self, feature, classifier):
-        PredictableModel.__init__(self, feature=feature, classifier=classifier)
-        self.subject_names = []
+    def __init__(self, error_code, exception, status_code=None):
+        Exception.__init__(self)
+        self.status_code = 400
+        self.exception = exception
+        self.error_code = error_code
+        try:
+            self.message = errors[self.error_code]
+        except:
+            self.error_code = UNKNOWN_ERROR
+            self.message = errors[self.error_code]
+        if status_code is not None:
+            self.status_code = status_code
 
-    def update(self, image, name):
-        c = self.__resolve_subject_id(name)
-        Y = self.feature.extract(image)
-        self.classifier.update(Y,c)
+    def to_dict(self):
+        rv = dict()
+        rv['status'] = 'failed'
+        rv['code'] = self.error_code
+        rv['message'] = self.message
+        return rv
 
-    def predict_image(self, image):
-        y = self.predict(image)
-        return self.__resolve_subject_name(y[0])
-        
-    def __resolve_subject_id(self, query_name):
-        # Do we have it in the list?
-        for pos in xrange(len(self.subject_names)):
-            if self.subject_names[pos] == query_name:
-                return pos
-        # If not, add it!
-        self.subject_names.append(query_name)
-        return len(self.subject_names) - 1
+# Wow, a decorator! This enables us to catch Exceptions 
+# in a method and raise a new WebAppException with the 
+# original Exception included. This is a quick and dirty way
+# to minimize error handling code in our server.
+class ThrowsWebAppException(object):
+   def __init__(self, error_code, status_code=None):
+      self.error_code = error_code
+      self.status_code = status_code
 
-    def __resolve_subject_name(self, query_id):
-        if len(self.subject_names) == 0:
-            raise WebAppException("No subjects available!")
-        return self.subject_names[query_id]
+   def __call__(self, function):
+      def returnfunction(*args, **kwargs):
+         try:
+            return function(*args, **kwargs)
+         except Exception as e:
+            raise WebAppException(self.error_code, e)
+      return returnfunction
 
-# Instantiate the model. This is horrible, because everything is lost with a Server restart. :-)
-model = UpdatableModel(feature=SpatialHistogram(lbp_operator=ExtendedLBP()), classifier=NearestNeighbor(dist_metric=ChiSquareDistance(), k=1))
+# Register an error handler on the WebAppException, so we
+# can return the error as JSON back to the User. At the same
+# time you should do some logging, so it doesn't pass by 
+# silently.
+@app.errorhandler(WebAppException)
+def handle_exception(error):
+    app.logger.exception(error.exception)
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
+# Now finally add the methods needed for our FaceRecognition API!
+# Right now there is no rate limiting, no auth tokens and so on.
+# 
+@ThrowsWebAppException(error_code = IMAGE_DECODE_ERROR)
 def read_image(base64_image):
-    """ Base64 decode the data, read it with PIL and convert it to grayscale.
+    """ Decodes Base64 image data, reads it with PIL and converts it into grayscale.
+
+    Args:
+    
+        base64_image [string] A Base64 encoded image (all types PIL supports).
     """
     enc_data = base64.b64decode(base64_image)
     file_like = cStringIO.StringIO(enc_data)
     im = Image.open(file_like)
-    return im.convert("L")
+    im = im.convert("L")
+    return im
 
-def resize_image(image, sz):
-    """ Resizes an image, so it doesn't exceed the maximum size given in sz. This function
-    sucks, because it doesn't respect any image ration. Just ignore it for this prototype.
-    """
-    (width, height) = image.size
-    if width > sz[0] or height > sz[1]:
-        new_width = min(sz[0], width)
-        new_height = min(sz[1], height)
-        image = image.resize(sz, Image.ANTIALIAS)
+def preprocess_image(image_data):
+    image = read_image(image_data)
     return image
 
-@app.route('/add', methods=["POST"])
-def add():
-    if request.headers['Content-Type'] == 'application/json':
-        try:
-            values = request.json
-            # Read the image:
-            image = read_image(values['image'])
-            image = resize_image(image, IMG_MAX_SIZE)
-            # And update the model:
-            subject_name = values['name']
-            model.update(image,subject_name)
-            return Response(status=201, mimetype='application/json')
-        except:
-            return Response(status=400, mimetype='application/json')
-        
-@app.route('/predict', methods=["POST"])
-def predict():
-    if request.headers['Content-Type'] == 'application/json':
-        try:
-            values = request.json
-            # Read the image:
-            image = read_image(values['image'])
-            image = resize_image(image, IMG_MAX_SIZE)
-            # Get the predicted name
-            prediction = model.predict_image(image)
-            return jsonify(name = prediction) 
-        except:
-            return Response(status=400, mimetype='application/json')         
+# Get the prediction from the global model.
+@ThrowsWebAppException(error_code = PREDICTION_ERROR)
+def get_prediction(image_data):
+    image = preprocess_image(image_data)
+    prediction = model.predict(image)
+    return prediction
 
+# Now add the API endpoints for recognizing, learning and 
+# so on. If you want to use this in any public setup, you
+# should add rate limiting, auth tokens and so on.
+@app.route('/api/recognize', methods=['GET', 'POST'])
+def identify():
+    if request.headers['Content-Type'] == 'application/json':
+            try:
+                image_data = request.json['image']
+            except:
+                raise WebAppException(error_code=MISSING_ARGUMENTS)
+            prediction = get_prediction(image_data)
+            response = jsonify(name = prediction) 
+            return response
+    else:
+        raise WebAppException(error_code=INVALID_FORMAT)
+
+# And now let's do this!
 if __name__ == '__main__':
+    # A long description:
+    long_description = ("server.py is a simple facerec webservice. It provides "
+        "you with a simple RESTful API to recognize faces from a "
+        "computed model. Please don't use this server in a production"
+        "environment, as it provides no security and there might be "
+        "ugly concurrency issues with the global state of the model." )
+    usage = "server.py <options> <model_filename.pkl>"
 
-    handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+    print "=== Description ==="
+    print long_description
+    print "=== Usage ==="
+    print "Usage:", usage
+    print "=== Server Log (also in %s) ===" % (LOG_FILENAME)
+    # Parse the command line:    
+    from optparse import OptionParser
 
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+    parser = OptionParser(usage=usage)
+    parser.add_option("-t", "--train", action="store", type="string", dest="dataset", default=None, 
+        help="Calculates a new model from a given CSV file. CSV format: <person>;</path/to/image/folder>.")
+     # Split between options and arguments
+    (options, args) = parser.parse_args()
+    # Check if a model filename was passed:
+    if len(args) == 0:
+        print "Expected a facerec model to use for recognition!"
+        sys.exit()    
+    # The filename of the model:
+    model_filename = args[0]
+    # Uh, this is ugly...
+    global model
+    # If a DataSet is given, we want to work with it:
+    if options.dataset:
+        # Learn the new model with the dataset given:
+        model = recognition.get_model_from_csv(filename=options.dataset,out_model_filename=model_filename)
+    else:
+        model = recognition.load_model_file(model_filename)
+    # Finally start the server:        
+    app.run(host="0.0.0.0", port=int("5000"), debug=True, use_reloader=False, threaded=False)
--- py/facerec/dataset.py
+++ py/facerec/dataset.py
@@ -36,6 +36,11 @@
 # new class, which we call a NumericDataSet. This NumericDataSet
 # allows us to add images and turn them into a facerec compatible
 # representation.
+#
+# This DataSet does not provide a method for removing entities yet,
+# because this would render the ordering of the labels useless. 
+# This is caused by a severe limitation of the framework right now,
+# because it would make algorithms like LDA and PCA fail.
 class NumericDataSet(object):
     def __init__(self):
         self.data = {}
